985050758822d8f8cbfe4fd08d3613e2f5642001
maxteebabs <maxteetechnologies@gmail.com>
2017-06-24 12:49:23 +0100

    changes

--- myapp/app/Http/Helpers/EmailHelpers.php
+++ myapp/app/Http/Helpers/EmailHelpers.php
@@ -81,7 +81,7 @@ final class EmailHelpers {
 	}
     //admin sending the email
     public function send() {
-        $mail = $this->initializeMail();
+        $mail = $this->initializeMail();dd($this->subject);
         $mail->subject = $this->subject;    
         $mail->MsgHTML($this->body);
         if($this->is_admin) {
